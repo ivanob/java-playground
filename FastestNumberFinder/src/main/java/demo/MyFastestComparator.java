74a06ab870edbf40fb7a4633b795f568facdfbb4
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2018-05-17 22:44:16 +0100

    First commit

---
+++ FastestNumberFinder/src/main/java/demo/MyFastestComparator.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.List;
+
+public class MyFastestComparator implements NumberFinder{
+    public boolean contains(int valueToFind, List<CustomNumberEntity> list) {
+        return false;
+    }
+
+    public List<CustomNumberEntity> readFromFile(String filePath) {
+        return null;
+    }
+}
